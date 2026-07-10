abc06a9151ca0021cd39b27ca28647921a55b29b
yashthakur19 <yashtomar0807@gmail.com>
2026-07-11 00:27:33 +0530

    made somechanges

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const cors=require('cors');
 const port=process.env.PORT || 5000;
+const rooms=[];
 app.use(express.json());
 app.use(cors());
 app.get('/',(req,res)=>{
@@ -9,8 +10,14 @@ app.get('/',(req,res)=>{
 });
 app.post('/api/room',(req,res)=>{
     const roomData=req.body;
+    rooms.push(roomData);
+
     console.log('Received room data:', roomData);
     res.status(200).json({ message: 'Room data received successfully' });
+});
+app.get('/api/room',(req,res)=>{
+    res.status(200).json(rooms);
+    console.log('Sending room data:', rooms);   
 });
 app.listen(port, ()=>{
     console.log('Server is running on port',port);
